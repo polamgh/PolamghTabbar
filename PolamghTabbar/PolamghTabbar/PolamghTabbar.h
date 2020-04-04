5e90519693d0ab52ecc5738f713565527ca92ae2
Ali Ghanavati <aliali3684@gmail.com>
2020-04-04 12:37:48 +0430

    First Commeit

---
+++ PolamghTabbar/PolamghTabbar/PolamghTabbar.h
@@ -0,0 +1,19 @@
+//
+//  PolamghTabbar.h
+//  PolamghTabbar
+//
+//  Created by Ali Ghanavati on 1/16/1399 AP.
+//  Copyright © 1399 AP Ali Ghanavati. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PolamghTabbar.
+FOUNDATION_EXPORT double PolamghTabbarVersionNumber;
+
+//! Project version string for PolamghTabbar.
+FOUNDATION_EXPORT const unsigned char PolamghTabbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PolamghTabbar/PublicHeader.h>
+
+
